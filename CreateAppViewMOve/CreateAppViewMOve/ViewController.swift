22b1404ccdbd17ebd01a4caa137f0cef92637c35
SonNV-D1 <sonnguyenv.mta@gmail.com>
2019-03-25 11:22:23 +0700

    createApp

---
+++ CreateAppViewMOve/CreateAppViewMOve/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CreateAppViewMOve
+//
+//  Created by SonNV-D1 on 3/25/19.
+//  Copyright © 2019 SonNV-D1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
